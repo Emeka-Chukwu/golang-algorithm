8df2a35bba6c44814cd21fa2ff7fb1ddd6fcf674
emeka-chukwu <emekapascal11@gmail.com>
2023-09-28 18:14:30 +0100

    updates

--- two_pointers/solution.go
+++ two_pointers/solution.go
@@ -14,4 +14,13 @@ func (al AlTwoPointers) Run() {
 	fmt.Println("gotten result :", al.isPalindrome("0P"), "      expected result : ", "false")
 	fmt.Println("End  isPalindrome ============")
 	fmt.Println()
+
+	fmt.Println("Start sum of two ============")
+
+	fmt.Println("gotten result :", al.twoSum([]int{2, 7, 11, 15}, 9), "     expected result : ", "[1 2]")
+	fmt.Println("gotten result :", al.twoSum([]int{2, 3, 4}, 6), "     expected result : ", "[1 3")
+	fmt.Println("gotten result :", al.twoSum([]int{-1, 0}, -1), "       expected result : ", "[1 2]")
+
+	fmt.Println("End  sum of two ============")
+	fmt.Println()
 }
